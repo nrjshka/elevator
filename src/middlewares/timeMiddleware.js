b2d42681f4514d1ff800b12b94dcddd611cd6226
Maxim Korolev <nrjshka@gmail.com>
2019-01-04 16:48:39 +0300

    Added: comment for time middleware

--- src/middlewares/timeMiddleware.js
+++ src/middlewares/timeMiddleware.js
@@ -18,27 +18,44 @@ export default store => next => action => {
       const { queue, elevator } = store.getState();
       const { currentFloor, isOpen } = elevator;
 
+      // Если на этом этаже вызывали лифт, то открываем двери, 
+      // чтобы загрузить пассажиров и поехать дальше
       if (queue.includes(currentFloor)) {
         if (!isOpen) {
           store.dispatch({ type: ELEVATOR.DOORS.OPEN });
         } else {
           store.dispatch({ type: ELEVATOR.DOORS.CLOSE });
 
+          // Чистим очередь ожидающих этажей после "загрузки" пассажиров
           store.dispatch({ type: QUEUE.DELETE, floor: currentFloor });
 
+          // Если это был последний этаж, то останавливаем таймер и лифт
           if (queue.length === 1) {
             store.dispatch({ type: TIMER.END });
+
+            store.dispatch({
+              type: ELEVATOR.CHANGE_STATUS,
+              status: ELEVATOR.MOVING_TO.STAYING,
+            });
           } else {
+            // Если это был не последний этаж, то меняем статус движения лифта и отправляем его двигаться
             const currentFloorId = queue.indexOf(currentFloor);
             const nextFloor = (currentFloorId > 0) ? queue[0] : queue[1];
+            const isElevatorMovingToTop = nextFloor > currentFloor;
+
+            store.dispatch({
+              type: ELEVATOR.CHANGE_STATUS,
+              status: isElevatorMovingToTop ? ELEVATOR.MOVING_TO.TOP : ELEVATOR.MOVING_TO.BOTTOM,
+            });
 
             store.dispatch({
               type: ELEVATOR.MOVE,
-              floor: nextFloor > currentFloor ? 1 : -1,
+              floor: isElevatorMovingToTop ? 1 : -1,
             });
           }
         }
       } else {
+        // Если никто не вызывал лифт, то просто двигаемся дальше
         store.dispatch({
           type: ELEVATOR.MOVE,
           floor: queue[0] > currentFloor ? 1 : -1,
